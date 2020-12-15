1538788525479ec8b87f884b6c11ab6dd63a4927
meiian <70202223+meiian@users.noreply.github.com>
2020-12-15 18:32:12 +0100

    added Konosuba and bigot display fixes

--- animejs.js
+++ animejs.js
@@ -1,6 +1,8 @@
 function resize() {
     if(screen.width < 600){
-        document.getElementById("leftcontainer").style.height = document.getElementById("rightcontainer").scrollHeight;
+        document.getElementById("rightcontainer").style.paddingTop = document.getElementById("leftinfos").scrollHeight;
+        document.getElementById("leftcontainer").style.height = document.getElementById("rightcontainer").scrollHeight + window.innerHeight*0.05;
+        
     }
     else {
         document.getElementById("syno").style.width = document.getElementById("rightcontainer").scrollWidth - document.getElementById("return").scrollWidth - document.getElementById("leftinfos").scrollWidth;
